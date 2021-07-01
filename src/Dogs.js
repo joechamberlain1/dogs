0c8b7c977d18ea8acb26e24176cd86411bf7f97e
joechamberlain1 <joechamberlain2801@gmail.com>
2021-07-01 21:44:42 +1200

    first commit

---
+++ src/Dogs.js
@@ -0,0 +1,112 @@
+import { Button, makeStyles } from "@material-ui/core";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import "./dogs.css";
+import Dropdown from "./Dropdown";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "noWrap",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+
+  gridList: {
+    width: 1080,
+    height: 305,
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
+
+const Dogs = () => {
+  const [dog, setDog] = useState([]);
+
+  useEffect(() => {
+    axios
+      .request("https://dog.ceo/api/breeds/image/random/1")
+      .then(function (response) {
+        console.log(response.data.message);
+        setDog(response.data.message);
+      })
+
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  const tenDogs = () => {
+    axios
+      .request("https://dog.ceo/api/breeds/image/random/10")
+      .then(function (response) {
+        console.log(response.data.message);
+        setDog(response.data.message);
+      })
+
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const twentyDogs = () => {
+    axios
+      .request("https://dog.ceo/api/breeds/image/random/20")
+      .then(function (response) {
+        console.log(response.data.message);
+        setDog(response.data.message);
+      })
+
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+  const FiftyDogs = () => {
+    axios
+      .request("https://dog.ceo/api/breeds/image/random/50")
+      .then(function (response) {
+        console.log(response.data.message);
+        setDog(response.data.message);
+      })
+
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const classes = useStyles();
+  return (
+    <>
+      <div className="dogs">
+        <h1>A Gallery of Good Dogs</h1>
+
+        <div className="button-container">
+          <Button variant="contained" onClick={() => tenDogs()}>
+            Fetch 10 Dogs
+          </Button>
+          <Button variant="contained" onClick={() => twentyDogs()}>
+            Fetch 20 Dogs
+          </Button>
+          <Button variant="contained" onClick={() => FiftyDogs()}>
+            Fetch 50 Dogs
+          </Button>
+        </div>
+        <div className={classes.root}>
+          <GridList cellHeight={300} className={classes.gridList} cols={4}>
+            {dog.map((dogs) => (
+              <GridListTile cols={1}>
+                <img key={dogs.id} src={dogs} alt="a very good dog" />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+        {/* <Dropdown /> */}
+      </div>
+    </>
+  );
+};
+
+export default Dogs;
---
+++ src/Dropdown.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "noWrap",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+
+  gridList: {
+    width: 1080,
+    height: 305,
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
+
+const Dropdown = () => {
+  const classes = useStyles();
+
+  const [breed, setBreed] = useState([]);
+  const [bulldog, setBulldog] = useState([]);
+
+  //   useEffect(() => {
+  //     axios
+  //       .get("https://dog.ceo/api/breeds/list/all")
+  //       .then(function (response) {
+  //         console.log(response.data.message.bulldog);
+  //         setBreed(response.data.message.bulldog);
+  //       })
+
+  //       .catch(function (error) {
+  //         console.error(error);
+  //       });
+  //   }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://dog.ceo/api/breed/bulldog/english/images/random/1")
+      .then(function (response) {
+        console.log(response.data.message);
+        setBulldog(response.data.message);
+      })
+
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://dog.ceo/api/breed/bulldog/french/images/random/1")
+      .then(function (response) {
+        console.log(response.data.message);
+        setBulldog(response.data.message);
+      })
+
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  const bostonBulldog = () => {
+    axios
+      .get("https://dog.ceo/api/breed/bulldog/boston/images/random/1")
+      .then(function (response) {
+        console.log(response.data.message);
+        setBulldog(response.data.message);
+      })
+
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  return (
+    <>
+      <form>
+        <label for="bulldogs">Choose your favourite bulldog!</label>
+        <select name="bulldogs" id="bulldogs">
+          {/* {breed.map((breeds) => (
+            <option key={breeds.id}>{breeds}</option>
+          ))} */}
+          <option> English</option>
+          <option> French</option>
+          <option> Boston</option>
+        </select>
+      </form>
+      <GridList cellHeight={300} className={classes.gridList} cols={4}>
+        {bulldog.map((bulldogs) => (
+          <GridListTile cols={1}>
+            <img key={bulldogs.id} src={bulldogs} alt="a very good dog" />
+          </GridListTile>
+        ))}
+      </GridList>
+    </>
+  );
+};
+
+export default Dropdown;
